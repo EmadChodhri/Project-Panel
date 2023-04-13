0e3b28adf9b8ea1082391d90a1bdb40f3dd17723
Dev1321 <125664041+Dev1321@users.noreply.github.com>
2023-04-13 16:14:55 +0500

    project added

---
+++ Assets/PanelManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject[] panels;
+    public Button[] buttons; 
+    
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void active()
+    {   
+        for (int i = 0; i < panels.Length; i++)
+        {
+            buttons[0].onClick.AddListener(() => panels[0].SetActive(true));
+            buttons[1].onClick.AddListener(() => panels[1].SetActive(true));
+            buttons[2].onClick.AddListener(() => panels[2].SetActive(true));
+            buttons[3].onClick.AddListener(() => panels[3].SetActive(true));
+            panels[i].SetActive(false);
+        }
+    }
+}
